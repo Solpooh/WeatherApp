ff36329656c693cbab72ce204a6f23158d42ffd0
Solpooh <hsw9420@naver.com>
2024-02-08 18:05:39 +0900

    [feat] Weather App GUI 추가

--- src/main/java/com/example/weatherappgui/AppLauncher.java
+++ src/main/java/com/example/weatherappgui/AppLauncher.java
@@ -5,19 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
+public class AppLauncher {
     public static void main(String[] args) {
-        launch();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // display our weather app gui
+                new WeatherAppGui().setVisible(true);
+            }
+        });
     }
 }
