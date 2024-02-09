d068d9ca9487d8afe85b24b6773bf6b4b77532b2
Solpooh <hsw9420@naver.com>
2024-02-09 11:37:46 +0900

    [feat] Weather API에서 날씨 데이터를 원하는 데이터 형식으로 추출

--- src/main/java/com/example/weatherappgui/AppLauncher.java
+++ src/main/java/com/example/weatherappgui/AppLauncher.java
@@ -14,9 +14,11 @@ public class AppLauncher {
             @Override
             public void run() {
 //                // display our weather app gui
-//                new WeatherAppGui().setVisible(true);
+                new WeatherAppGui().setVisible(true);
 
-                System.out.println(WeatherApp.getLocationData("Tokyo"));
+//                System.out.println(WeatherApp.getLocationData("Tokyo"));
+
+                System.out.println(WeatherApp.getCurrentTime());
             }
         });
     }
